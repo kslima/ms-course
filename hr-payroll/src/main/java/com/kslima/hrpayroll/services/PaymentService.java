44d871474de96f5f504ad08742755ff36e3a94ea
Kleuder Lima <kleuder.lima@gmail.com>
2021-09-22 05:37:13 -0300

    Rest template

--- hr-payroll/src/main/java/com/kslima/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/kslima/hrpayroll/services/PaymentService.java
@@ -1,12 +1,30 @@
 package com.kslima.hrpayroll.services;
 
 import com.kslima.hrpayroll.entities.Payment;
+import com.kslima.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Kleuder de Souza Lima", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id",  Long.toString(workerId));
+
+        String url = workerHost + "/workers/{id}";
+        Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
